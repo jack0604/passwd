720ac42641924ba5650a5406df4d042ac3487490
Ezio-auditore <83087211+jack0604@users.noreply.github.com>
2023-04-20 10:05:26 +0200

    Add files via upload

---
+++ crittografia_file.py
@@ -0,0 +1,16 @@
+import os
+def crittografia():
+    while True:
+        os.system('clear')
+        os.system('figlet Crittografia')
+        password = "sasso"
+        passwd = input("\nenter password --------> ")
+        if passwd == password:
+            os.system('clear')
+            print("ok perfetto")
+            os.system('python3 password.py')
+            exit()
+        else:
+            os.system('clear')
+            print("Password errata")
+crittografia()
---
+++ password.py
@@ -0,0 +1,29 @@
+import os
+import webbrowser
+import time
+def credenziali():
+    os.system('figlet Password')
+    #inserimento Dati
+    print("---------------------->")
+    nome = input("inserisci il tuo Username: ")
+    print("---------------------->")
+    passwd = input("inserisci la password: ")
+    os.system('clear')
+    #Creazione file
+    print(" * ")
+    time.sleep(0.6)
+    print(" * *  ")
+    time.sleep(0.6)
+    print(" * * * ")
+    time.sleep(0.6)
+    print(" * * * * ")
+    time.sleep(4)
+    f = open("Guida.txt","w")
+    f.write("per maggiori info sulla password")
+    f.write("\nPassword è koala")
+    f.close()
+    time.sleep(10)
+    os.remove("Guida.txt")
+    os.system('clear')
+    os.system('figlet arrivederci ' + nome)
+credenziali()
